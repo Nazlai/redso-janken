873892ac11a7408d656c9cabf77eb86eb4c6a57b
Joe Ho <joebcrmd@gmail.com>
2021-04-07 23:00:30 +0800

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./app";
+import "normalize.css";
+import "styles/app";
+
+const root = document.getElementById("root");
+
+render(<App />, root);
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const webpackConfig = {
+  entry: "/src/app/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+    alias: {
+      app: path.resolve(__dirname, "src/app"),
+      components: path.resolve(__dirname, "src/app/components"),
+      screens: path.resolve(__dirname, "src/app/screens"),
+      firebaseUtils: path.resolve(__dirname, "src/app/firebase"),
+      routes: path.resolve(__dirname, "src/app/routes"),
+      styles: path.resolve(__dirname, "src/assets/stylesheets"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.module\.scss$/i,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss|css$/i,
+        exclude: /\.module\.scss$/i,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "url-loader",
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new Dotenv({ path: "./.env", systemvars: true }),
+    new HtmlWebpackPlugin({
+      title: "Janken",
+      template: "./src/app/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = webpackConfig;
