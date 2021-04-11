a30c30df8f12dcb89eb2a7c304f7c1a45257e1e2
Joe Ho <joebcrmd@gmail.com>
2021-04-12 01:38:34 +0800

    add styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "theme/muiTheme";
 import App from "./app";
@@ -10,6 +11,7 @@ const root = document.getElementById("root");
 
 render(
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
   </ThemeProvider>,
   root
