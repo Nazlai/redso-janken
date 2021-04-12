71c65193c46adc63e57cdc3aad4f253e7ad28864
Joe Ho <joebcrmd@gmail.com>
2021-04-12 17:21:41 +0800

    use card for display

--- src/app/screens/gameList/index.js
+++ src/app/screens/gameList/index.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState, Fragment } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Typography } from "@material-ui/core";
-import { db } from "firebaseUtils";
-import { JOIN_GAME } from "constants/routes";
-
-// FIXME
-// use card for created games
-// extract firebase method
+import {
+  Container,
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { withNavigation } from "components/navigation";
+import { db, auth, cloudFunctions } from "firebaseUtils";
+import { PLAY_GAME } from "constants/routes";
+import useStyles from "./style";
 
 const GameList = () => {
+  const classes = useStyles();
   const [list, setList] = useState([]);
   const history = useHistory();
 
@@ -23,30 +30,64 @@ const GameList = () => {
     return subscribe;
   }, []);
 
-  const routeJoinGame = (gameId) => {
-    const location = {
-      pathname: JOIN_GAME,
-      search: `gameId=${gameId}`,
-    };
-    history.push(location);
+  const handleJoinGame = (gameId) => {
+    const { uid } = auth.currentUser;
+
+    cloudFunctions
+      .joinGame({ gameId: gameId, playerId: uid })
+      .then(() => {
+        const location = {
+          pathname: PLAY_GAME,
+          search: `gameId=${gameId}`,
+        };
+        history.push(location);
+      })
+      .catch(console.log);
   };
 
   return (
-    <div>
-      <Typography align="center" variant="h2">
-        All Games
-      </Typography>
-      {list.length ? (
-        list.map((game) => (
-          <Fragment key={game.gameId}>
-            <p onClick={() => routeJoinGame(game.gameId)}>{game.gameId}</p>
-          </Fragment>
-        ))
-      ) : (
-        <p>no current games</p>
-      )}
-    </div>
+    <Container>
+      <Box
+        minHeight="calc(100vh - 64px)"
+        m={1}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {list.length ? (
+          list.map((game) => {
+            const { gameId, players, finished } = game;
+            const gameStatus = finished ? "Finished" : "In progress";
+            const playerCount = players.length;
+            return (
+              <Card key={gameId} className={classes.card}>
+                <CardContent>
+                  <Typography align="left">GameID: {gameId}</Typography>
+                  <Typography align="left">
+                    Game Status: {gameStatus}
+                  </Typography>
+                  <Typography align="left">
+                    Current Players: {playerCount}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    color="primary"
+                    onClick={() => handleJoinGame(gameId)}
+                  >
+                    join
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          })
+        ) : (
+          <Typography className={classes.text}>No current games</Typography>
+        )}
+      </Box>
+    </Container>
   );
 };
 
-export default GameList;
+export default withNavigation(GameList);
