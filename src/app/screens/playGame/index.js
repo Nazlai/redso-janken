843cb31512e71191b5f1808543abe61e6efbdc67
Joe Ho <joebcrmd@gmail.com>
2021-04-12 20:20:18 +0800

    fix play game layout

--- src/app/screens/playGame/index.js
+++ src/app/screens/playGame/index.js
@@ -1,67 +1,111 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, Redirect } from "react-router-dom";
+import Qrcode from "qrcode.react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHandRock,
   faHandPaper,
   faHandScissors,
+  faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
-import { db, auth, cloudFnApi } from "firebaseUtils";
-import { ROCK, PAPER, SCISSOR } from "module/rockPaperScissors";
-import style from "./playGame.module";
+import {
+  Button,
+  Box,
+  Container,
+  Grid,
+  Typography,
+  Snackbar,
+  IconButton,
+} from "@material-ui/core";
+import { withNavigation } from "components/navigation";
+import { db, auth, cloudFunctions } from "firebaseUtils";
+import { ROCK, PAPER, SCISSOR, getWinText } from "module/rockPaperScissors";
+import useStyles from "./style";
 import { LANDING, JOIN_GAME } from "constants/routes";
+import { SimpleDialog } from "components/dialog";
+import PropTypes from "prop-types";
 
-// FIXME
-// extract firebase methods
-// if user is not present in game send him to join
-const Icon = ({ children, className }) => (
-  <span className={className}>{children}</span>
+const displayWinText = (state) => {
+  if (state.finished && !state.isDraw) {
+    return getWinText[state.winningWeapon];
+  }
+  return null;
+};
+
+const displayGameStatus = (state, uid) => {
+  const { finished, isDraw, winners } = state;
+  if (!finished || !uid) return "Waiting for other players...";
+
+  const didPlayerWin = winners.includes(uid) ? "You won!" : "You lost!";
+  const message = isDraw ? "It's a draw!" : didPlayerWin;
+  return message;
+};
+
+const PlayGameButton = ({ children, handleClick, ...rest }) => (
+  <Button
+    variant="contained"
+    color="secondary"
+    fullWidth
+    onClick={handleClick}
+    {...rest}
+  >
+    {children}
+  </Button>
 );
 
-const Draw = () => <p>draw</p>;
-const DisplayWinner = ({ winners, playerId }) => {
-  const message = winners.includes(playerId) ? "you win" : "you lost";
-  return <p>{message}</p>;
+PlayGameButton.propTypes = {
+  children: PropTypes.string,
+  handleClick: PropTypes.func,
 };
 
-const Result = ({ isDraw, ...rest }) => {
-  const Element = isDraw ? Draw : DisplayWinner;
-  return <Element {...rest} />;
-};
-
-const Game = (props) => {
+const PlayGame = () => {
   const [state, setState] = useState({});
+  const [showDialog, setShowDialog] = useState(false);
+  const [showToast, setShowToast] = useState({ display: false, message: "" });
   const [weapon, setWeapon] = useState("");
+  const classes = useStyles({ weapon });
   const [redirectUrl, setRedirect] = useState(null);
   const location = useLocation();
   const gameId = new URLSearchParams(location.search).get("gameId");
+  const shareGameUrl = `${window.location.origin}${JOIN_GAME}?gameId=${gameId}`;
+  const uid = auth && auth.currentUser && auth.currentUser.uid;
 
   useEffect(() => {
     const subscribe = db
       .collection("games")
       .doc(gameId)
       .onSnapshot((snapshot) => {
-        const { finished, isDraw, winners, players } = snapshot.data();
+        const {
+          finished,
+          isDraw,
+          winners,
+          players,
+          winningWeapon,
+          restarted,
+        } = snapshot.data();
+        console.log(snapshot.data());
         const isPlayerPresent = players.includes(auth.currentUser.uid);
         if (!isPlayerPresent) {
           setRedirect(`${JOIN_GAME}?gameId=${gameId}`);
         }
-        setState({ finished, isDraw, winners });
+        setState({ finished, isDraw, winners, winningWeapon });
+        if (restarted) {
+          setWeapon("");
+        }
       });
     return subscribe;
   }, []);
 
-  const leaveGame = () => {
-    const cloudFn = cloudFnApi.leaveGame();
+  const handleLeaveGame = () => {
     const { uid } = auth.currentUser;
 
-    cloudFn({ gameId, playerId: uid })
+    cloudFunctions
+      .leaveGame({ gameId, playerId: uid })
+      .then((response) => response.data)
       .then((message) => console.log(message))
       .catch(console.log);
   };
 
-  // FIXME
-  // what if user picked a weapon but refreshed browser and picked again?
   const handleConfirm = () => {
     const { uid } = auth.currentUser;
     const player = {
@@ -69,20 +113,29 @@ const Game = (props) => {
       player: uid,
       weapon: weapon,
     };
-    const cloudFn = cloudFnApi.commitMove();
 
-    cloudFn(player)
-      .then((data) => console.log(data, "call"))
+    cloudFunctions
+      .commitMove(player)
+      .then((response) => response.data)
+      .then((message) => {
+        if (message.warning) {
+          setShowToast({ display: true, message: message.message });
+          setWeapon(message.move);
+        }
+      })
       .catch(console.error);
   };
 
-  const newGame = () => {
-    const cloudFn = cloudFnApi.newGame();
-
-    cloudFn({ gameId })
-      .then(console.log)
+  const handleRestartGame = () => {
+    cloudFunctions
+      .newGame({ gameId })
+      .then((response) => response.data)
+      .then((message) =>
+        setShowToast({ display: true, message: message.message })
+      )
       .catch((error) => console.log(error, "error"));
   };
+  const hideToast = () => setShowToast({ message: "", display: false });
 
   if (!gameId) {
     return <Redirect to={LANDING} />;
@@ -93,41 +146,122 @@ const Game = (props) => {
   }
 
   return (
-    <div>
-      {state.finished ? (
-        <Result {...state} playerId={auth.currentUser.uid} />
-      ) : null}
-      <div className={style.container}>
-        <Icon className={weapon === ROCK ? style.active : style.icon}>
-          <FontAwesomeIcon icon={faHandRock} onClick={() => setWeapon(ROCK)} />
-        </Icon>
-        <Icon className={weapon === PAPER ? style.active : style.icon}>
-          <FontAwesomeIcon
-            icon={faHandPaper}
-            onClick={() => setWeapon(PAPER)}
-          />
-        </Icon>
-        <Icon className={weapon === SCISSOR ? style.active : style.icon}>
-          <FontAwesomeIcon
-            icon={faHandScissors}
-            rotation={90}
-            onClick={() => setWeapon(SCISSOR)}
-          />
-        </Icon>
-      </div>
-      <button type="button" onClick={handleConfirm} disabled={weapon === ""}>
-        Confirm
-      </button>
-      {state.finished ? (
-        <button type="button" onClick={newGame}>
-          new game
-        </button>
-      ) : null}
-      <button type="button" onClick={leaveGame}>
-        leave game
-      </button>
-    </div>
+    <Container>
+      <Box height="calc(70vh - 64px)">
+        <Box p={2}>
+          <Button
+            color="secondary"
+            className={classes.text}
+            onClick={() => setShowDialog(true)}
+          >
+            share
+          </Button>
+        </Box>
+        <Box
+          p={2}
+          minHeight={200}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h3" className={classes.feedback} gutterBottom>
+            {displayGameStatus(state, uid)}
+          </Typography>
+          <Typography className={classes.feedback}>
+            {displayWinText(state)}
+          </Typography>
+        </Box>
+        {state.finished ? null : (
+          <Box p={2}>
+            <Grid container>
+              <Grid item xs={4}>
+                <Button onClick={() => setWeapon(ROCK)}>
+                  <FontAwesomeIcon icon={faHandRock} className={classes.rock} />
+                </Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button onClick={() => setWeapon(PAPER)}>
+                  <FontAwesomeIcon
+                    icon={faHandPaper}
+                    className={classes.paper}
+                  />
+                </Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button onClick={() => setWeapon(SCISSOR)}>
+                  <FontAwesomeIcon
+                    icon={faHandScissors}
+                    rotation={90}
+                    className={classes.scissor}
+                  />
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        )}
+      </Box>
+      <Box
+        height="30vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Box width="70%">
+          {state.finished ? (
+            <React.Fragment>
+              <Box mb={2}>
+                <PlayGameButton handleClick={handleRestartGame}>
+                  restart game
+                </PlayGameButton>
+              </Box>
+              <PlayGameButton handleClick={handleLeaveGame}>
+                leave game
+              </PlayGameButton>
+            </React.Fragment>
+          ) : (
+            <PlayGameButton
+              handleClick={handleConfirm}
+              disabled={weapon === ""}
+            >
+              Choose
+            </PlayGameButton>
+          )}
+        </Box>
+      </Box>
+      <SimpleDialog
+        title="Share Join Game Link"
+        open={showDialog}
+        onClose={() => setShowDialog(false)}
+      >
+        <Box m={2}>
+          <Qrcode value={shareGameUrl} />
+        </Box>
+        <Box>
+          <Typography gutterBottom>{shareGameUrl}</Typography>
+        </Box>
+      </SimpleDialog>
+      <Snackbar
+        open={showToast.display}
+        message={showToast.message}
+        autoHideDuration={6000}
+        onClose={hideToast}
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={hideToast}
+            >
+              <FontAwesomeIcon icon={faTimesCircle} />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </Container>
   );
 };
 
-export default Game;
+export default withNavigation(PlayGame);
