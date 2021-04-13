17e330225fa7c629ded0004a6f72fd67c1a5857b
Joe Ho <joebcrmd@gmail.com>
2021-04-13 12:49:16 +0800

    fix hide button if game doc is not found

--- src/app/screens/playGame/index.js
+++ src/app/screens/playGame/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, Redirect } from "react-router-dom";
+import { useLocation, Redirect, Link } from "react-router-dom";
 import Qrcode from "qrcode.react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,6 +7,8 @@ import {
   faHandPaper,
   faHandScissors,
   faTimesCircle,
+  faUserPlus,
+  faListAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   Button,
@@ -17,13 +19,11 @@ import {
   Snackbar,
   IconButton,
 } from "@material-ui/core";
-import { withNavigation } from "components/navigation";
 import { db, auth, cloudFunctions } from "firebaseUtils";
 import { ROCK, PAPER, SCISSOR, getWinText } from "module/rockPaperScissors";
 import useStyles from "./style";
-import { LANDING, JOIN_GAME } from "constants/routes";
-import { SimpleDialog } from "components/dialog";
-import PropTypes from "prop-types";
+import { LANDING, JOIN_GAME, GAME_LIST } from "constants/routes";
+import { CenterBox, FullButton, SimpleDialog, Navigation } from "components";
 
 const displayWinText = (state) => {
   if (state.finished && !state.isDraw) {
@@ -41,25 +41,9 @@ const displayGameStatus = (state, uid) => {
   return message;
 };
 
-const PlayGameButton = ({ children, handleClick, ...rest }) => (
-  <Button
-    variant="contained"
-    color="secondary"
-    fullWidth
-    onClick={handleClick}
-    {...rest}
-  >
-    {children}
-  </Button>
-);
-
-PlayGameButton.propTypes = {
-  children: PropTypes.string,
-  handleClick: PropTypes.func,
-};
-
 const PlayGame = () => {
   const [state, setState] = useState({});
+  const [hideControls, setHideControls] = useState(false);
   const [showDialog, setShowDialog] = useState(false);
   const [showToast, setShowToast] = useState({ display: false, message: "" });
   const [weapon, setWeapon] = useState("");
@@ -70,29 +54,41 @@ const PlayGame = () => {
   const shareGameUrl = `${window.location.origin}${JOIN_GAME}?gameId=${gameId}`;
   const uid = auth && auth.currentUser && auth.currentUser.uid;
 
+  if (!gameId) {
+    return <Redirect to={LANDING} />;
+  }
+
   useEffect(() => {
     const subscribe = db
       .collection("games")
       .doc(gameId)
-      .onSnapshot((snapshot) => {
-        const {
-          finished,
-          isDraw,
-          winners,
-          players,
-          winningWeapon,
-          restarted,
-        } = snapshot.data();
+      .onSnapshot(
+        (snapshot) => {
+          if (snapshot.exists) {
+            const {
+              finished,
+              isDraw,
+              winners,
+              players,
+              winningWeapon,
+              restarted,
+            } = snapshot.data();
 
-        const isPlayerPresent = players.includes(auth.currentUser.uid);
-        if (!isPlayerPresent) {
-          setRedirect(`${JOIN_GAME}?gameId=${gameId}`);
-        }
-        setState({ finished, isDraw, winners, winningWeapon, players });
-        if (restarted) {
-          setWeapon("");
-        }
-      });
+            const isPlayerPresent = players.includes(auth.currentUser.uid);
+            if (!isPlayerPresent) {
+              setRedirect(`${JOIN_GAME}?gameId=${gameId}`);
+            }
+            if (restarted) {
+              setWeapon("");
+            }
+            setState({ finished, isDraw, winners, winningWeapon, players });
+          } else {
+            setHideControls(true);
+            setShowToast({ display: true, message: "Game not found" });
+          }
+        },
+        (error) => console.log(error)
+      );
     return subscribe;
   }, []);
 
@@ -135,138 +131,128 @@ const PlayGame = () => {
       )
       .catch((error) => console.log(error, "error"));
   };
+
   const hideToast = () => setShowToast({ message: "", display: false });
 
-  if (!gameId) {
-    return <Redirect to={LANDING} />;
-  }
+  const renderMoves = () => {
+    if (state.finished) return null;
+
+    return (
+      <Grid container>
+        <Grid item xs={4}>
+          <Button onClick={() => setWeapon(ROCK)}>
+            <FontAwesomeIcon icon={faHandRock} className={classes.rock} />
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button onClick={() => setWeapon(PAPER)}>
+            <FontAwesomeIcon icon={faHandPaper} className={classes.paper} />
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button onClick={() => setWeapon(SCISSOR)}>
+            <FontAwesomeIcon
+              icon={faHandScissors}
+              rotation={90}
+              className={classes.scissor}
+            />
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  };
+
+  const renderButtons = () => {
+    return state.finished ? (
+      <React.Fragment>
+        <Box mb={2}>
+          <FullButton handleClick={handleRestartGame}>restart game</FullButton>
+        </Box>
+        <FullButton handleClick={handleLeaveGame}>leave game</FullButton>
+      </React.Fragment>
+    ) : (
+      <FullButton handleClick={handleConfirm} disabled={weapon === ""}>
+        Choose
+      </FullButton>
+    );
+  };
 
   if (redirectUrl) {
     return <Redirect to={redirectUrl} />;
   }
 
   return (
-    <Container>
-      <Box height="calc(70vh - 64px)">
-        <Box p={2}>
-          <Button
-            color="secondary"
-            className={classes.text}
-            onClick={() => setShowDialog(true)}
-          >
-            invite other players
-          </Button>
-        </Box>
-        <Box
-          p={2}
-          minHeight={200}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Typography variant="h3" className={classes.feedback} gutterBottom>
-            {displayGameStatus(state, uid)}
-          </Typography>
-          <Typography className={classes.feedback}>
-            {displayWinText(state)}
-          </Typography>
-          {state.players ? (
-            <Typography className={classes.feedback}>
-              Players in room: {state.players.length}
-            </Typography>
-          ) : null}
-        </Box>
-        {state.finished ? null : (
-          <Box p={2}>
-            <Grid container>
-              <Grid item xs={4}>
-                <Button onClick={() => setWeapon(ROCK)}>
-                  <FontAwesomeIcon icon={faHandRock} className={classes.rock} />
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button onClick={() => setWeapon(PAPER)}>
-                  <FontAwesomeIcon
-                    icon={faHandPaper}
-                    className={classes.paper}
-                  />
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button onClick={() => setWeapon(SCISSOR)}>
-                  <FontAwesomeIcon
-                    icon={faHandScissors}
-                    rotation={90}
-                    className={classes.scissor}
-                  />
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        )}
-      </Box>
-      <Box
-        height="30vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Box width="70%">
-          {state.finished ? (
-            <React.Fragment>
-              <Box mb={2}>
-                <PlayGameButton handleClick={handleRestartGame}>
-                  restart game
-                </PlayGameButton>
-              </Box>
-              <PlayGameButton handleClick={handleLeaveGame}>
-                leave game
-              </PlayGameButton>
-            </React.Fragment>
-          ) : (
-            <PlayGameButton
-              handleClick={handleConfirm}
-              disabled={weapon === ""}
-            >
-              Choose
-            </PlayGameButton>
-          )}
-        </Box>
-      </Box>
-      <SimpleDialog
-        title="Invite Players With Link"
-        open={showDialog}
-        onClose={() => setShowDialog(false)}
-      >
-        <Box m={2}>
-          <Qrcode value={shareGameUrl} />
-        </Box>
+    <React.Fragment>
+      <Navigation>
         <Box>
-          <Typography gutterBottom>{shareGameUrl}</Typography>
+          <IconButton onClick={() => setShowDialog(true)}>
+            <FontAwesomeIcon icon={faUserPlus} className={classes.navIcon} />
+          </IconButton>
+          <IconButton>
+            <Link to={GAME_LIST} className={classes.link}>
+              <FontAwesomeIcon icon={faListAlt} className={classes.navIcon} />
+            </Link>
+          </IconButton>
         </Box>
-      </SimpleDialog>
-      <Snackbar
-        open={showToast.display}
-        message={showToast.message}
-        autoHideDuration={6000}
-        onClose={hideToast}
-        action={
-          <React.Fragment>
-            <IconButton
-              size="small"
-              aria-label="close"
-              color="inherit"
-              onClick={hideToast}
-            >
-              <FontAwesomeIcon icon={faTimesCircle} />
-            </IconButton>
-          </React.Fragment>
-        }
-      />
-    </Container>
+      </Navigation>
+      <Container>
+        <Box height="70vh - 64px">
+          <CenterBox m={4} minHeight="5rem">
+            <Typography className={classes.feedback}>
+              GameID: {gameId}
+            </Typography>
+            {state.players && (
+              <Typography className={classes.feedback}>
+                Players in room: {state.players.length}
+              </Typography>
+            )}
+          </CenterBox>
+          <CenterBox margin={4} minHeight="30vh">
+            <Typography variant="h3" className={classes.feedback} gutterBottom>
+              {displayGameStatus(state, uid)}
+            </Typography>
+            <Typography className={classes.feedback}>
+              {displayWinText(state)}
+            </Typography>
+          </CenterBox>
+          <Box m={4}>{hideControls ? null : renderMoves()}</Box>
+        </Box>
+        <CenterBox>
+          <Box width="70%" maxWidth="450px">
+            {hideControls ? null : renderButtons()}
+          </Box>
+        </CenterBox>
+        <SimpleDialog
+          title="Invite Players With Link"
+          open={showDialog}
+          onClose={() => setShowDialog(false)}
+        >
+          <Box m={2}>
+            <Qrcode value={shareGameUrl} />
+          </Box>
+          <Typography gutterBottom>{shareGameUrl}</Typography>
+        </SimpleDialog>
+        <Snackbar
+          open={showToast.display}
+          message={showToast.message}
+          autoHideDuration={6000}
+          onClose={hideToast}
+          action={
+            <React.Fragment>
+              <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={hideToast}
+              >
+                <FontAwesomeIcon icon={faTimesCircle} />
+              </IconButton>
+            </React.Fragment>
+          }
+        />
+      </Container>
+    </React.Fragment>
   );
 };
 
-export default withNavigation(PlayGame);
+export default PlayGame;
